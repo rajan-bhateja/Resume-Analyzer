1dc8943436db1c355fc645f0861adc60fd9ff318
rajan <rajanbhateja6@gmail.com>
2025-05-14 17:42:18 +0530

    Added PyPDFLoader

--- analyzer.py
+++ analyzer.py
@@ -1,4 +1,6 @@
 import streamlit as st
+from langchain_community.document_loaders import PyPDFLoader
+import tempfile
 
 st.set_page_config(layout="wide", page_title="Resume Analyzer")
 
@@ -8,8 +10,15 @@ st.caption("Powered by Langchain and Gemini")
 try:
     uploaded_resume = st.file_uploader(label="Upload your Resume:", type="pdf")
 
-    if uploaded_resume.name.endswith(".pdf"):
-        st.info("Working on it!")
+    if uploaded_resume is not None:
+        with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as tmp_file:
+            tmp_file.write(uploaded_resume.read())
+            tmp_file_path = tmp_file.name
 
-except:
-    st.error("An error occurred!\n \nPlease upload the resume in .pdf format only!")
+        st.subheader("Resume Content:")
+        loader = PyPDFLoader(file_path=tmp_file_path, extraction_mode="layout", mode="page")
+        docs = loader.load()
+        st.write(docs[15].page_content)
+
+except Exception as e:
+    st.error(f"An error occurred: {e}")
