bf3d1c9f22c2cabb40dbbba01bb743bc706f419a
rajan <rajanbhateja6@gmail.com>
2026-01-26 23:56:12 +0530

    Utility functions added

--- backend/utilities.py
+++ backend/utilities.py
@@ -1,8 +1,10 @@
 import re
+import uuid
+from datetime import datetime, timezone
+
 
 def validate_email(email: str) -> str:
     """Validates email address"""
-
     pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
     if not re.match(pattern, email):
         raise ValueError(f"Invalid email address: {email}")
@@ -12,11 +14,41 @@ def validate_email(email: str) -> str:
 
 def validate_resume_extension(resume_filename: str) -> str:
     """Validates resume extension"""
-
     if not resume_filename.lower().endswith((".pdf", ".docx")):
-        raise TypeError(
+        raise ValueError(
             f"Unsupported resume extension: {resume_filename}"
             f"\nSupported extensions: .pdf, .docx"
         )
 
     return resume_filename
+
+
+def validate_content_type(content_type: str) -> str:
+    """Validates content type"""
+    if content_type.lower() not in ("application/pdf", "application/vnd.openxmlformats-officedocument.wordprocessingml.document"):
+        raise ValueError(f"Invalid content type: {content_type}")
+
+    return content_type
+
+
+def validate_file_size(file_size_bytes: int, max_size_bytes: int = 5242880) -> int:
+    """Validates file size in bytes."""
+    if file_size_bytes > max_size_bytes:
+        raise ValueError("File size cannot be greater than 5 MB")
+
+    return file_size_bytes
+
+
+def generate_request_id() -> str:
+    """Generates a request id"""
+    return str(uuid.uuid4())
+
+
+def get_time_utc() -> str:
+    """Gets current time in UTC"""
+    return str(datetime.now(timezone.utc))
+
+
+def normalize_string(string: str) -> str:
+    """Normalizes string"""
+    return string.strip().lower()
--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI, UploadFile, File, Form, HTTPException
-from backend.utilities import validate_email, validate_resume_extension
+from backend.utilities import (
+    validate_email,
+    validate_resume_extension,
+    validate_content_type,
+    # validate_file_size,
+    generate_request_id,
+    get_time_utc
+)
+
 
 app = FastAPI(title="Resume Analyzer")
-
 
 @app.get("/status")
 async def get_status() -> dict[str, str]:
@@ -13,10 +20,17 @@ async def get_status() -> dict[str, str]:
 async def analyze_resume(
     email: str = Form(...),
     resume: UploadFile = File(...)
-):
+    ):
     try:
+        # contents = await resume.read()
+        # file_size_bytes = len(contents)
+        # validated_file_size = validate_file_size(file_size_bytes)
         validated_email = validate_email(email)
         validated_filename = validate_resume_extension(resume.filename)
+        validated_content_type = validate_content_type(resume.content_type)
+        uuid_str = generate_request_id()
+
+        # resume.file.seek(0)     # Place the pointer to the beginning of the file
 
     except ValueError as e:
         raise HTTPException(status_code=400, detail=str(e))
@@ -24,5 +38,8 @@ async def analyze_resume(
     return {
         "email": validated_email,
         "filename": validated_filename,
-        "content_type": resume.content_type
+        "content_type": validated_content_type,
+        # "file_size": validated_file_size,
+        "uuid": uuid_str,
+        "timestamp": get_time_utc()
     }
